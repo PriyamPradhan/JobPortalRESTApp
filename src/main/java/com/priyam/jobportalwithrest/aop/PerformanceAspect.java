32438fc8b2751f788d7ca2f1947c4dde4d646807
Priyam Pradhan <priyampradhan07@gmail.com>
2024-08-12 00:43:40 +0530

    added AOP of @Around

---
+++ src/main/java/com/priyam/jobportalwithrest/aop/PerformanceAspect.java
@@ -0,0 +1,30 @@
+package com.priyam.jobportalwithrest.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerformanceAspect {
+
+    //added logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceAspect.class);
+
+    @Around("execution(* com.priyam.jobportalwithrest.service.JobService.getAllJobs(..)))")
+    public Object monitorTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object obj = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+
+        LOGGER.info("Time taken for " + joinPoint.getSignature().getName() + " to process execution time: " + (endTime - startTime) + "ms");
+
+    return obj;
+    }
+}
